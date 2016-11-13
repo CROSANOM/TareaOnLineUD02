42dc7f9e2cf0b8d9cd254ed38379aa243c7372cc
crosanom <crosanom@hotmail.com>
2016-11-13 19:05:06 +0100

    Saludo impersonal

---
+++ src/saluda.java
@@ -0,0 +1,13 @@
+
+public class saluda {
+
+	public saluda() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hola caracola");
+	}
+
+}
