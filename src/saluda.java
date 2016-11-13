911d4e457fcfb3d86e1461ac33b81a9956cb998a
crosanom <crosanom@hotmail.com>
2016-11-13 20:15:36 +0100

    2º commit saludo personal

--- src/saluda.java
+++ src/saluda.java
@@ -1,13 +1,19 @@
-
+import java.util.Scanner;/*
+ **
+importamos la clase escanner para leer por teclado*/
 public class saluda {
-
 	public saluda() {
 		// TODO Auto-generated constructor stub
 	}
-
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String nombre;
+		System.out.println("Hola como te llamas, introduce tu nombre humana");
+		nombre=sc.nextLine();
+		System.out.println("Buenos dias:"+" "+ nombre);
+		
 		// TODO Auto-generated method stub
-System.out.println("hola caracola");
+		
 	}
 
 }
